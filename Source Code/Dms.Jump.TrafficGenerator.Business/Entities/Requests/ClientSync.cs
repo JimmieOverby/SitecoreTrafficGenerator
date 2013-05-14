52032dbd473b0b31ae3c55e152cc3423f0505868
Anton Tishchenko <antontishchenko@gmail.com>
2013-05-14 16:49:34 +0300

    Added correct generation of visits with cookies
    To be sure that Sitecore handle single session user correctly, after
    first request we get Sitecore cookies(ASP.Net Session value) and other
    requests use this cookies

---
+++ Source Code/Dms.Jump.TrafficGenerator.Business/Entities/Requests/ClientSync.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net;
+
+namespace Dms.Jump.TrafficGenerator.Business.Entities.Requests
+{
+    /// <summary>
+    /// The class for getting Sitecore cookies after request.
+    /// </summary>
+    public class ClientSync
+    {
+        /// <summary>
+        /// The perform request and return cookies.
+        /// </summary>
+        /// <param name="uri">
+        /// The uri.
+        /// </param>
+        /// <param name="cookieContainer">
+        /// The cookie container.
+        /// </param>
+        /// <returns>
+        /// The <see cref="CookieContainer"/>.
+        /// </returns>
+        public static CookieContainer PerformRequest(Uri uri, CookieContainer cookieContainer)
+        {
+            var newCookies = cookieContainer;
+
+            // Create the request object.
+            var request = (HttpWebRequest)WebRequest.Create(uri);
+            if (cookieContainer != null)
+            {
+                request.CookieContainer = cookieContainer;
+            }
+
+            var response = (HttpWebResponse)request.GetResponse();
+            newCookies.Add(response.Cookies);
+
+            response.Close();
+            return newCookies;
+        }
+    }
+}
